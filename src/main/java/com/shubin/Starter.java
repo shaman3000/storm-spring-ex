e2cdbfd7cbc6782788a0272b1ebb79698413a3d2
ssh <shaman_3000@mail.ru>
2016-11-10 10:16:08 +0300

    start

---
+++ src/main/java/com/shubin/Starter.java
@@ -0,0 +1,15 @@
+package com.shubin;
+
+/**
+ * Created by sshubin on 10.11.2016.
+ */
+
+public class Starter {
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello");
+    }
+
+
+}
